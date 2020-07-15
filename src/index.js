3da8c5d446ecf130e8a92164ebd956a94f9b01e7
Avinash Kumar <avinash@ahaloans.com>
2020-07-15 22:47:57 +0530

    add comment

--- src/index.js
+++ src/index.js
@@ -5,15 +5,22 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
+        // Initiaze state
         this.state = { lat: null };
-    };
-    render() {
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                console.log(position);
+                
+                this.setState({lat: position.coords.latitude})
+            },
             (err) => console.log(err)       
             
         );
-        return <div>Latitude: </div>
+    };
+    render() {
+        
+    return <div>Latitude: {this.state.lat}</div>
     }
 }
 ReactDOM.render(
