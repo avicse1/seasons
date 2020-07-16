7312657d8572df7c4c318eafb1d27748161f5e44
Avinash Kumar <avinash@ahaloans.com>
2020-07-16 13:39:57 +0530

    displaying winter or summer text on the screen

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    if(month > 3 && month < 7) {
+        return lat > 0 ? 'Summer' : 'Winter';
+    }else {
+        return lat > 0 ? 'Winter' : 'Summer';
+    }
+};
 const SeasonDisplay = (props) => {
-    console.log(props.lat);
-    
-    return <div>Season Display !</div>
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'Winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    return(
+        <div><h1>{text}</h1></div>
+    );
 };
 
 export default SeasonDisplay;
